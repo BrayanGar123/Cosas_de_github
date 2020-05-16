097ff9a2a6655b9b2cee1c531e330426f8158eb4
BrayanGar123 <b.garcia@uniandes.edu.co>
2020-05-16 17:31:52 -0400

    Tarea de GitHub

---
+++ factorial/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int factorial(int a){
+    if(a==0 || a==1){
+        return 1;
+    }
+    return a*factorial(a-1);
+}
+int main(){
+    std::cout << "Hola profesor" << std::endl ;
+    int n;
+    cout << "Por favor introduce un numero entero" << endl;
+    cin >> n;
+    
+    cout << "El factorial es "<< factorial(n) <<endl;
+    return 0;
+}
